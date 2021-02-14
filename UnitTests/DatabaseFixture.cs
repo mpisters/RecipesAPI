8c352004ca1275fb6cb09e6865a62393a9484171
mpisters <pistersmichelle@gmail.com>
2021-02-14 15:26:54 +0100

    add tests for post recipes

---
+++ UnitTests/DatabaseFixture.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Recipes.Models;
+using Recipes.UnitTests;
+using Xunit;
+
+namespace Recipes.UnitTests
+{
+    public class DatabaseFixture: IDisposable
+    {
+        public DatabaseFixture()
+        {
+            options = new DbContextOptionsBuilder<RecipesContext>()
+                .UseInMemoryDatabase(databaseName: "TestRecipeDatabase")
+                .Options;
+        }
+        
+        public DbContextOptions<RecipesContext> options { get; set; }
+
+        public void Dispose()
+        {
+            this.options = null;
+        }
+    }
+}
+
+[CollectionDefinition("Database collection")]
+public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
--- UnitTests/TestPostRecipes.cs
+++ UnitTests/TestPostRecipes.cs
@@ -1,22 +1,26 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Recipes.Controllers;
 using Recipes.Models;
 using Xunit;
 
 namespace Recipes.UnitTests
 {
+    
+    [Collection("Database collection")]
     public class TestPostRecipes
     {
+        private readonly DatabaseFixture _fixture;
+
+        public TestPostRecipes(DatabaseFixture fixture)
+        {
+            this._fixture = fixture;
+        }
         [Fact]
         public async Task PostRecipeWithoutIngredients()
         {
-            // Todo place this in a setup class/fixture
-            var options = new DbContextOptionsBuilder<RecipesContext>()
-                .UseInMemoryDatabase(databaseName: "TestRecipeDatabase")
-                .Options;
+            var options = _fixture.options;
             var recipesContext = new RecipesContext(options);
 
             var controller = new RecipesController(recipesContext);
@@ -24,11 +28,49 @@ namespace Recipes.UnitTests
 
             var newRecipe = new RecipeDto("Test recipe", "Test description", ingredientList);
             await controller.PostRecipe(newRecipe);
-            Assert.Single(recipesContext.Recipes.ToListAsync().Result);
-            var addedRecipe = recipesContext.Recipes.FirstOrDefault();
+            var addedRecipe = recipesContext.Recipes.Last();
             Assert.Equal("Test recipe", addedRecipe.Name);
             Assert.Equal("Test description", addedRecipe.Description);
             Assert.Empty(addedRecipe.Ingredients);
+        }
+
+        [Fact]
+        public async Task PostRecipeWithIngredient()
+        {
+            var options = _fixture.options;
+            var recipesContext = new RecipesContext(options);
+
+            var controller = new RecipesController(recipesContext);
+            var newIngredient = new IngredientDto("ui", 2, "unit");
+            var ingredientList = new[] {newIngredient};
+
+            var newRecipe = new RecipeDto("Test recipe", "Test description", ingredientList);
+            await controller.PostRecipe(newRecipe);
+            var addedRecipe = recipesContext.Recipes.LastOrDefault();
+            Assert.Equal("Test recipe", addedRecipe.Name);
+            Assert.Equal("Test description", addedRecipe.Description);
+            var firstIngredient = addedRecipe.Ingredients[0];
+            Assert.Equal("ui", firstIngredient.Name);
+            Assert.Equal(2, firstIngredient.Amount);
+            Assert.Equal("unit", firstIngredient.Unit);
+        }
+        [Fact]
+        public async Task PostRecipeWithIngredients()
+        {
+            var options = _fixture.options;
+            var recipesContext = new RecipesContext(options);
+
+            var controller = new RecipesController(recipesContext);
+            var newIngredient = new IngredientDto("ui", 2, "unit");
+            var newIngredient2 = new IngredientDto("knoflook", 3, "unit");
+            var ingredientList = new[] {newIngredient, newIngredient2};
+
+            var newRecipe = new RecipeDto("Test recipe", "Test description", ingredientList);
+            await controller.PostRecipe(newRecipe);
+            var addedRecipe = recipesContext.Recipes.Last();
+            Assert.Equal("Test recipe", addedRecipe.Name);
+            Assert.Equal("Test description", addedRecipe.Description);
+            Assert.Equal(2, addedRecipe.Ingredients.Count);
         }
     }
 }
